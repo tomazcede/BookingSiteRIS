9b6956e93d70054a68b264bbd33e54ec6c0211f1
tomazcede <tomi.cede1@gmail.com>
2024-05-20 14:14:36 +0200

    Rezervacije zdej delajo

--- BookingSite/Controllers/ListingController.cs
+++ BookingSite/Controllers/ListingController.cs
@@ -15,9 +15,13 @@ public class ListingController : Controller
         _context = new BookingDatabaseContext();
     }
 
-    public async Task<IActionResult> Index(int id)
+    public async Task<IActionResult> Index(int id, Boolean reserved_err = false, Boolean reserved_succ = false)
     {
-        var listing = await _context.Listingis.Include(u => u.Neprimicnina)
+        if(reserved_err)
+            TempData["msg"] = "<script>alert('Invalid date interval, check the other reservations');</script>";
+        if(reserved_succ)
+            TempData["msg"] = "<script>alert('Reservation added successfully');</script>";
+        var listing = await _context.Listingis.Include(u => u.Neprimicnina).Include(u => u.Rezervacijes)
             .FirstOrDefaultAsync(m => m.ListingId == id);
         if (listing == null)
         {
@@ -32,10 +36,48 @@ public class ListingController : Controller
             .ToListAsync();
         return View(listing);
     }
+    
+    public async Task<IActionResult> BookListing(int id, string from_date, string till_date)
+    {
+        if(!SignedIn())
+            return RedirectToAction("Login", "Uporabniki");
+        
+        var reservations = await _context.Rezervacijes.Where(d => d.ListingId == id).ToListAsync();
+        DateTime from = DateTime.Parse(from_date);
+        DateTime till = DateTime.Parse(till_date);
+
+        foreach (var reservation in reservations)
+        {
+            if (reservation.DatumOd <= from && reservation.DatumDo >= from)
+                return RedirectToAction("Index", new { id = id, reserved_err = true });
+            if (reservation.DatumOd <= till && reservation.DatumDo >= till)
+                return RedirectToAction("Index", new { id = id, reserved_err = true });
+        }
+
+        Rezervacije r = new Rezervacije();
+        r.ListingId = id;
+        r.UporabnikId = _context.Uporabnikis.FirstOrDefault(d => d.Email == HttpContext.User.Identity.Name).UporabnikId;
+        r.DatumOd = from;
+        r.DatumDo = till;
+        r.RezervacijaId = _context.Rezervacijes.Count() + 1;
+
+        _context.Rezervacijes.Add(r);
+        _context.SaveChanges();
+        
+        return RedirectToAction("Index", new { id = id, reserved_succ=true });
+    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+    
+    private bool SignedIn()
+    {
+        if(HttpContext.User.Identity.Name != null){
+            return true;
+        }
+        return false;
     }
 }
--- BookingSite/obj/Debug/net8.0/BookingSite.AssemblyInfo.cs
+++ BookingSite/obj/Debug/net8.0/BookingSite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookingSite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17644f0ec9d50a9ef92babd421d3c735f5ea09e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b448f4f91117ab6c69d5ac8ea01925503b85c5ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookingSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookingSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
