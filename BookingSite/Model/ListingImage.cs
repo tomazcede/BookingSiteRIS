522ba5857832d23615a5e947270d802acf0e26ae
Jakob <jakob.simcic0@gmail.com>
2024-05-22 01:23:10 +0200

    prenova

---
+++ BookingSite/Model/ListingImage.cs
@@ -0,0 +1,10 @@
+﻿using BookingSite.Model;
+
+public class ListingImage
+{
+    public int Id { get; set; }
+    public int ListingId { get; set; }
+    public string Url { get; set; }
+
+    public virtual Listingi Listing { get; set; }
+}
